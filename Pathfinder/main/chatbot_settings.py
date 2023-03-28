7e7b7068812ef69273787f581966a98af92c1000
Dean Logan <61757204+DeanLogan123@users.noreply.github.com>
2023-03-28 16:58:32 +0100

    Fixed issue where my_corpus.yml wasn't being found within chatbot_settings.py

--- Pathfinder/main/chatbot_settings.py
+++ Pathfinder/main/chatbot_settings.py
@@ -1,14 +1,18 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
 
 chatbot = ChatBot('MyChatBot')
 
 trainer = ChatterBotCorpusTrainer(chatbot)
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATASET_PATH = os.path.join(ROOT_DIR, 'data\my_corpus.yml')
+
 # Train the chatbot with the English corpus
 #trainer.train("chatterbot.corpus.english")
 #trainer.train("chatterbot.corpus.english.greetings")
-trainer.train("main.data.my_corpus")
+trainer.train(DATASET_PATH)
 
 def get_response(msg):
     return str(chatbot.get_response(msg))

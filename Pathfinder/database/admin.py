592af84b56b69b0c0689c49c8a366fb48236c16f
Dean Logan <61757204+DeanLogan123@users.noreply.github.com>
2023-04-10 18:19:04 +0100

    Created admin portal, then created an admin that has access to user accounts and db's, then created a db admin that has access to entire db.

--- Pathfinder/database/admin.py
+++ Pathfinder/database/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Module);
+admin.site.register(Assessment);
+admin.site.register(Lecturer);
+admin.site.register(ModuleLecturer);
+admin.site.register(Pathway);
+admin.site.register(ModulePathway);
+admin.site.register(Student);
+admin.site.register(StudentInfo);
+admin.site.register(StudentModule);
+admin.site.register(StudentInterest);

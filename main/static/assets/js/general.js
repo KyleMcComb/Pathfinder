529f8b0588c0319b2ecfef3a7aa73417f334690b
Ross McAllister <mcallister2002@btinternet.com>
2023-11-11 00:09:53 +0000

    Fixed ui elements to work on mobile: Font sizes based off screen size. Nav bar updated to be simplified. Chatbot page configured to work with simplified nav bar. Chatbot page updated to use new column layout. Chatbot textarea attached to bottom of the screen. Removed need for js to resize content div on chatbot page.

--- main/static/assets/js/general.js
+++ main/static/assets/js/general.js
@@ -9,7 +9,6 @@ function pageLoad() {
     // Ensure the navigation sidebar is closed whenever the page loads and adjust UI elements
     sessionStorage.setItem('navOpen', 'true');
     navToggle();
-    resizeContentDiv();
 
     // Check the theme and font size settings, and resize the page accordingly
     checkTheme();
@@ -65,7 +64,7 @@ function checkTheme() {
 }
 
 /**
- * @Author - @DeanLogan
+ * @Author - @RossMcAlliste + @DeanLogan
  * @Description - Checks the user's selected font size and applies it to the page's body element.
  * Adjusts the font size of the body element based on user preference.
  */
@@ -75,26 +74,22 @@ function checkFontSize() {
 
     // Check the selected font size and apply it to the page's body element
     if (fontSize == 'small') {
-        element.style.fontSize = '1.18vh'; // Apply the small font size
+        element.style.fontSize = '1.5vh'; // Apply the small font size
     } else if (fontSize == 'large') {
-        element.style.fontSize = '2.31vh'; // Apply the large font size
+        element.style.fontSize = '3vh'; // Apply the large font size
     } else {
-        element.style.fontSize = '1.48vh'; // Apply the default (medium) font size
+        element.style.fontSize = '2vh'; // Apply the default (medium) font size
     }
-
-    resiveClosebtn(); // Resize the close button (burger icon for nav toggle)
-    // The size of this icon depends on the sidebar width, not what the user has selected as their font size
 }
 
 
 /**
  * @Author - @DeanLogan
  * @Description - Toggles the side bar (nav) open or closed, and updates its state in session storage.
- * Adjusts UI elements by calling resizeNav and resizeContentDiv functions.
+ * Adjusts UI elements by calling resizeNav.
  */
 function navToggle() {
     var navOpen = sessionStorage.getItem('navOpen'); // Get the current state of the navigation sidebar from session storage
-
     // Close the navigation sidebar if it's currently open
     if (navOpen == 'true') {
         sessionStorage.setItem('navOpen', 'false'); // Update the navigation state to closed
@@ -105,106 +100,45 @@ function navToggle() {
     }
 
     // Resize the navigation and content elements based on the updated navigation state
-    resizeNav(sessionStorage.getItem('navOpen')); // Resize the navigation sidebar
-    resizeContentDiv(); // Resize the content div
+    resizeNav(); // Resize the navigation sidebar
 }
 
 
 /**
- * @Author - @DeanLogan
+ * @author - @RossMcAllister
  * @Description - Resizes the navigation sidebar based on its state (open or closed) and window width.
  * Adjusts the width and height of the sidebar and resizes the close button accordingly.
- * @param {string} navOpen - The state of the navigation sidebar ('true' for open, 'false' for closed).
  */
-function resizeNav(navOpen) {
-    var windowWidth = window.innerWidth; // Get the current window width
+function resizeNav() {
+    var column1Ele = document.getElementById('column1');
+    var column2Ele = document.getElementById('column2');
 
-    // Close the navigation sidebar
-    if (navOpen == 'false') {
-        // Check if the window width is smaller than 1117.1px
-        if (windowWidth < 1117.1) {
-            document.getElementsByClassName('sidebar')[0].style.width = '32px'; // Set small width for closed sidebar
-        } else {
-            document.getElementsByClassName('sidebar')[0].style.width = '2.864vw'; // Set responsive width for closed sidebar
-        }
+    //if column has closed class, then remove it and open nav
+    if (column1Ele.classList.contains('col-1-closed')) {
+        column1Ele.classList.add('col-1');
+        column1Ele.classList.remove('col-1-closed');
+
+        column2Ele.classList.add('col-2');
+        column2Ele.classList.remove('col-2-closed');
     }
-    // Open the navigation sidebar
-    else if (navOpen == "true") {
-        // Check if the window width is smaller than 1117.1px
-        if (windowWidth < 1117.1) {
-            document.getElementsByClassName('sidebar')[0].style.width = '145.22px'; // Set width for open sidebar
-        } else {
-            document.getElementsByClassName('sidebar')[0].style.width = '13vw'; // Set responsive width for open sidebar
-        }
+    else if (column1Ele.classList.contains('col-1')) { //or if it has regular class, remove ut and close nav
+        column1Ele.classList.add('col-1-closed');
+        column1Ele.classList.remove('col-1');
+
+        column2Ele.classList.add('col-2-closed');
+        column2Ele.classList.remove('col-2');
     }
-    
-    resiveClosebtn(); // Resize the close button (burger icon)
-    document.getElementsByClassName('sidebar')[0].style.height = '100vh'; // Set sidebar height to cover the entire viewport
-}
-
-
-/**
- * @Author - @DeanLogan
- * @Description - Resizes the close button (often called "burger" icon) based on window width.
- * Adjusts the font size of the close button for responsive design.
- */
-function resiveClosebtn() {
-    var windowWidth = window.innerWidth; // Get the current window width
-    var closebtn = document.getElementsByClassName('closebtn')[0]; // Get the close button element
-
-    // Check the window width and adjust the font size of the close button
-    if (windowWidth < 1117.1) {
-        closebtn.style.fontSize = '20.1076px'; // Set font size for small screens
-    } else {
-        closebtn.style.fontSize = '1.8vw'; // Set responsive font size for larger screens
+    else {
+        //error, column 1 does not have either of the correct classes.
+        console.log("Error: Resize navigation bar not working. Column 1 does not contain either of the correct classes.");
     }
 }
-
-/**
- * @Author - @DeanLogan
- * @Description - Resizes the content div based on the state of the navigation sidebar (open or closed) and window width.
- * Adjusts the width and position of the content div to fit the layout.
- */
-function resizeContentDiv() {
-    var contentDiv = document.getElementsByClassName('content')[0]; // Get the content div element
-    var windowWidth = window.innerWidth; // Get the current window width
-
-    var navOpen = sessionStorage.getItem('navOpen'); // Get the state of the navigation sidebar
-
-    // NAV IS OPEN
-    if (navOpen == 'true') {
-        // Check if the window width is smaller than 1117.1px
-        if (windowWidth < 1117.1) {
-            //contentDiv.style.width = (windowWidth - 145) + 'px'; // Set the width for open sidebar on small screens
-            contentDiv.style.left = '145px'; // Set the left position
-        } else {
-            //contentDiv.style.width = windowWidth - (windowWidth * 0.13) + 'px'; // Set responsive width for open sidebar
-            contentDiv.style.left = windowWidth * 0.13 + 'px'; // Set responsive left position
-        }
-    }
-    // NAV IS NOT OPEN
-    else if (navOpen == 'false') {
-        // Check if the window width is smaller than 1117.1px
-        if (windowWidth < 1117.1) {
-            //contentDiv.style.width = (windowWidth - 32) + 'px'; // Set the width for closed sidebar on small screens
-            contentDiv.style.left = '32px'; // Set the left position
-        } else {
-            //contentDiv.style.width = windowWidth - (windowWidth * 0.0286) + 'px'; // Set responsive width for closed sidebar
-            contentDiv.style.left = windowWidth * 0.0286 + 'px'; // Set responsive left position
-        }
-    }
-}
-
 
 /**
  * @Author - @DeanLogan
  * @Description - Holds event listeners that are applied to every web page.
  */
-function listeners() {
-    // When the window is resized, adjust UI elements
-    window.addEventListener('resize', function() { resizeContentDiv(); }); // Resize content div
-    window.addEventListener('resize', function() { resizeNav(sessionStorage.getItem('navOpen')); }); // Resize navigation sidebar
-    
+function listeners() {    
     // Menu animation code
     const menu = document.getElementById('menu');
     Array.from(document.getElementsByClassName('menu-item')).forEach((item, index) => {
